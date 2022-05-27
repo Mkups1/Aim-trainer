d78c80d787f278927478f1c69b0ff2bb916c3bdd
Mkups1 <m.kupilik1@share.epsb.ca>
2022-05-27 13:23:27 -0600

    Finished

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ let ctx = cnv.getContext("2d")
 cnv.width = 1250;
 cnv.height = 655;
 
-let change = 0
+let change = 1
 let number3 = document.getElementById("number-3")
 let number2 = document.getElementById("number-2")
 let number1 = document.getElementById("number-1")
 let scoreEl = document.getElementById("score")
 let score = 0
+let timeEl = document.getElementById("time")
+let time = 60
 let targetX = 600
 let targetY = 300
 
@@ -23,43 +25,40 @@ ctx.fillText("To start, press the start button", 425, 600)
 
 
 // When you click the start button, start the game
-document.getElementById("button").addEventListener("click", start)
+document.getElementById("button").addEventListener("click", () => {
+    change = 1
+    score = 0
+    time = 60
+    start()
+})
 
 
 function start(){
-    // if (change <= 4) {
-        setTimeout(start, 1000)
-        change++
-        console.log("change")
-    // } else {
-    //     change = 0
-    // }
+    console.log(change)
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, 1250, 655)
     if(change === 1){
-        ctx.fillStyle = "white"
-        ctx.fillRect(0, 0, 1250, 655)
+        change++
         ctx.drawImage(number3, 375, 25)
-    } 
-    if(change === 2){
-        ctx.fillStyle = "white"
-        ctx.fillRect(0, 0, 1250, 655)
+        setTimeout(start, 1000)
+    } else if(change === 2){
+        change++
         ctx.drawImage(number2, 400, 25, 400, 600)
-    } 
-    if(change === 3){
-        ctx.fillStyle = "white"
-        ctx.fillRect(0, 0, 1250, 655)
+        setTimeout(start, 1000)
+    } else if(change === 3){
+        change++
         ctx.drawImage(number1, 375, 25)
-
-    } 
-    if(change === 4){
-        ctx.fillStyle = "white"
-        ctx.fillRect(0, 0, 1250, 655)
+        setTimeout(start, 1000)
+    } else if (change === 4){
+        // change = 0
         ctx.fillStyle = "Blue"
         ctx.fillRect(targetX, targetY, 50, 50)
         document.addEventListener("mousedown", hit)
-    }
-
+        setInterval(timeF, 1000)
+    } 
 
 }
+
 
 
 function hit(event){
@@ -67,12 +66,30 @@ function hit(event){
     if(event.x - cnv.getBoundingClientRect().x  >= targetX && event.x - cnv.getBoundingClientRect().x <= targetX + 50 && event.y - cnv.getBoundingClientRect().y >= targetY && event.y - cnv.getBoundingClientRect().y <= targetY + 50){
         ctx.fillStyle = "white"
         ctx.fillRect(0, 0, 1250, 655)
-        targetX = (Math.random() * 1250)
-        targetY = (Math.random() * 655)
+        targetX = (Math.random() * 1200)
+        targetY = (Math.random() * 605)
         score++
         scoreEl.innerHTML = score
         ctx.fillStyle = "Blue"
         ctx.fillRect(targetX, targetY, 50, 50)
     }
 
+}
+
+function timeF(){
+    if(time <= 60 && time >=1)
+    time--
+    timeEl.innerHTML = time
+    console.log(time)
+    if (time === 0){
+        targetX += 100000
+        targetY += 100000
+        ctx.fillStyle = "white"
+        ctx.fillRect(0, 0, 1250, 655)
+        ctx.fillStyle="black"
+        ctx.font = "150px Arial"
+        ctx.fillText("Congratulations!", 90, 300)
+        ctx.font = "100px Arial"
+        ctx.fillText("Your score:" + score, 300, 500)
+    }
 }
